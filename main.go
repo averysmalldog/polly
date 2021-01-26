0522d09201b05cd235832f0ba82b1d94b617a01a
averysmalldog <averysmalldog@gmail.com>
2021-01-26 16:31:21 -0500

    go fmt

--- main.go
+++ main.go
@@ -17,22 +17,22 @@ import (
 // supply the IP of the wall conenctor.
 func InfluxAsyncGet(writeAPI *api.WriteAPI, wcIP string) {
 	client := *writeAPI
-    var data map[string]interface{}
+	var data map[string]interface{}
 
-    resp, err := http.Get(fmt.Sprintf("http://%s/api/1/vitals", wcIP))
-    if err != nil {
-        fmt.Println("error - during GET of hpwc. Do you have the right IP?")
-        return
-    }
+	resp, err := http.Get(fmt.Sprintf("http://%s/api/1/vitals", wcIP))
+	if err != nil {
+		fmt.Println("error - during GET of hpwc. Do you have the right IP?")
+		return
+	}
 
-    defer resp.Body.Close()
-    
+	defer resp.Body.Close()
+
 	body, _ := ioutil.ReadAll(resp.Body)
-    json.Unmarshal(body, &data)
-    
-    // Output a dot (.) for every successful GET against the Wall Connector
-    // This helps people like me who need to see something to know it works
-    fmt.Printf(".")
+	json.Unmarshal(body, &data)
+
+	// Output a dot (.) for every successful GET against the Wall Connector
+	// This helps people like me who need to see something to know it works
+	fmt.Printf(".")
 
 	p := influxdb2.NewPoint(
 		"hpwc",
@@ -49,13 +49,13 @@ func InfluxAsyncGet(writeAPI *api.WriteAPI, wcIP string) {
 func main() {
 	hpwcIP := os.Getenv("HPWC_IP")
 	client := influxdb2.NewClientWithOptions("http://localhost:8086", "my-token", influxdb2.DefaultOptions().SetBatchSize(20))
-    writeAPI := client.WriteAPI("admin", "tesla")
+	writeAPI := client.WriteAPI("admin", "tesla")
 
-    // The way this is set up, these likely don't get executed on ^C.
+	// The way this is set up, these likely don't get executed on ^C.
 	defer client.Close()
-    defer writeAPI.Flush()
-    
-    // Simple, isn't it?
+	defer writeAPI.Flush()
+
+	// Simple, isn't it?
 	for {
 		go InfluxAsyncGet(&writeAPI, hpwcIP)
 		time.Sleep(time.Millisecond * 1000)
