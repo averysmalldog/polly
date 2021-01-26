3b70a7f2b0a30bfa93832a72eeb567a1c9497faa
averysmalldog <averysmalldog@gmail.com>
2021-01-26 11:56:21 -0500

    implement as goroutines

--- main.go
+++ main.go
@@ -9,44 +9,46 @@ import (
 	"time"
 
 	"github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 )
 
-func AsyncGet()
+// InfluxAsyncGet polls the Tesla Gen3 Wall Connector and writes results to
+// InfluxDB with an aync, non-blocking client you supply. You must also
+// supply the IP of the wall conenctor.
+func InfluxAsyncGet(writeAPI *api.WriteAPI, wcIP string) {
+	client := *writeAPI
+    var data map[string]interface{}
+    resp, err := http.Get(fmt.Sprintf("http://%s/api/1/vitals", wcIP))
+    if err != nil {
+        fmt.Println("error - during GET of hpwc. Do you have the right IP?")
+        return
+    }
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &data)
+	fmt.Printf(".")
+	p := influxdb2.NewPoint(
+		"hpwc",
+		map[string]string{
+			"product":  "Gen3 HPWC",
+			"vendor":   "Tesla",
+			"location": "Garage",
+		},
+		data,
+		time.Now())
+
+	client.WritePoint(p)
+
+}
 
 func main() {
-	// Grab HPWC_IP from envvars:
 	hpwcIP := os.Getenv("HPWC_IP")
-    // Create a new client using an InfluxDB server base URL and an authentication token
-    // and set batch size to 20 
-    client := influxdb2.NewClientWithOptions("http://localhost:8086", "my-token",
-        influxdb2.DefaultOptions().SetBatchSize(20))
-    // Get non-blocking write client
-    writeAPI := client.WriteAPI("admin","tesla")
-    // write some points
-    for {
-		// query the HPWC for info
-		var data map[string]interface{}
-		resp, _ := http.Get(fmt.Sprintf("http://%s/api/1/vitals",hpwcIP))
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		json.Unmarshal(body, &data)
-		fmt.Printf("%+v\n", data)
-        // create point
-        p := influxdb2.NewPoint(
-            "hpwc",
-            map[string]string{
-                "product":  "Gen3 HPWC",
-                "vendor":   "Tesla",
-                "location": "Garage",
-            },
-            data,
-            time.Now())
-        // write asynchronously
-        writeAPI.WritePoint(p)
-		time.Sleep(time.Millisecond*1000)
-    }
-    // Force all unwritten data to be sent
-    writeAPI.Flush()
-    // Ensures background processes finishes
-    client.Close()
+	client := influxdb2.NewClientWithOptions("http://localhost:8086", "my-token", influxdb2.DefaultOptions().SetBatchSize(20))
+	writeAPI := client.WriteAPI("admin", "tesla")
+	defer client.Close()
+	defer writeAPI.Flush()
+	for {
+		go InfluxAsyncGet(&writeAPI, hpwcIP)
+		time.Sleep(time.Millisecond * 1000)
+	}
 }
